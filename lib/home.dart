ae439659c5a918f834bcf72db70b7d1deffd5192
Sami Bouakel <oxidsami492@gmail.com>
2023-01-08 19:03:10 +0100

    finishing the ui

--- lib/home.dart
+++ lib/home.dart
@@ -13,28 +13,231 @@ class _HomeState extends State<Home> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('C R U D  A P P'),
+          title: const Text('C R U D  A P P'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Create()));
+                },
+                child: const Text('Create'),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Read()));
+                },
+                child: const Text('Read'),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Update()));
+                },
+                child: const Text('Update'),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Delete()));
+                },
+                child: const Text('Delete'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Create extends StatefulWidget {
+  const Create({Key? key}) : super(key: key);
+
+  @override
+  State<Create> createState() => _CreateState();
+}
+
+class _CreateState extends State<Create> {
+  TextEditingController textEditingController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('C R E A T E'),
         ),
         body: Column(
           children: [
-            Container(),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('Create'),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('Read'),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('Update'),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('Delete'),
+            const SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: textEditingController,
+                decoration: InputDecoration(
+                  labelText: 'Item',
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: Colors.black,
+                      width: 2.0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: Colors.blue,
+                      width: 2.0,
+                    ),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.save),
+                    onPressed: () {
+                      textEditingController.text = '';
+                    },
+                  ),
+                ),
+              ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class Read extends StatefulWidget {
+  const Read({Key? key}) : super(key: key);
+
+  @override
+  State<Read> createState() => _ReadState();
+}
+
+class _ReadState extends State<Read> {
+  List<String> items = <String>[];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('R E A D'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(items[index]),
+                );
+              }),
+        ),
+      ),
+    );
+  }
+}
+
+class Update extends StatefulWidget {
+  const Update({Key? key}) : super(key: key);
+
+  @override
+  State<Update> createState() => _UpdateState();
+}
+
+class _UpdateState extends State<Update> {
+  bool editing = false;
+  List<String> items = <String>[];
+  TextEditingController textEditingController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('U P D A T E'),
+        ),
+        body: Visibility(
+          visible: editing,
+          replacement: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(items[index]),
+                    trailing: const Icon(
+                      Icons.edit,
+                      color: Colors.green,
+                    ),
+                  );
+                }),
+          ),
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: textEditingController,
+                  decoration: InputDecoration(
+                    labelText: 'Updated item',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Colors.black,
+                        width: 2.0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Colors.blue,
+                        width: 2.0,
+                      ),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: const Icon(Icons.check),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Delete extends StatefulWidget {
+  const Delete({Key? key}) : super(key: key);
+
+  @override
+  State<Delete> createState() => _DeleteState();
+}
+
+class _DeleteState extends State<Delete> {
+  List<String> items = <String>[];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('D E L E T E'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(items[index]),
+                  trailing: const Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                  ),
+                );
+              }),
         ),
       ),
     );
